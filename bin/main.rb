bb51b05a50fd2ba7adfc8489c556f44681af15a6
Bishoy-Samwel <bishoy.samwuel@gmail.com>
2021-03-05 17:28:35 +0200

    Initial Code

---
+++ bin/main.rb
@@ -0,0 +1,14 @@
+require 'telegram/bot'
+
+token = 'YOUR_TELEGRAM_BOT_API_TOKEN'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
