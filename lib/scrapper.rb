7fbf10036482ac5ce1533aefd65ceb2822ca949d
Bishoy-Samwel <bishoy.samwuel@gmail.com>
2021-03-09 00:48:41 +0200

    Add scrapper.rb add links of each category

---
+++ lib/scrapper.rb
@@ -0,0 +1,16 @@
+class Scrapper
+  @links = {
+    'Breakfast and brunch'=>'78/breakfast-and-brunch/', 
+    'Lunch'=> '17561/lunch/', 
+    'Dinners'=> '17562/dinner/', 
+    'Appetizer and Snacks' => '76/appetizers-and-snacks/', 
+    'Breads'=>'156/bread/'
+    'Drinks'=> '77/drinks/', 
+    'Salads'=> '96/salad/',
+    'Deserts'=> '79/desserts/',
+    'Side Dishes'=> '81/side-dish/',
+    'Soups and Stews' => '94/soups-stews-and-chili/'
+  }
+
+end
+end
