1d13f7fa2a121bfdc68576b0542fc6e1de7c29ab
Bishoy-Samwel <bishoy.samwuel@gmail.com>
2021-03-09 12:01:13 +0200

    Add 2 conditions if the message is a category or recipe

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,31 @@
 require 'telegram/bot'
+require_relative '../lib/app_config'
+require_relative '../lib/reply'
+require_relative '../lib/scrapper'
 
-token = 'YOUR_TELEGRAM_BOT_API_TOKEN'
+# token = 'YOUR_TELEGRAM_BOT_API_TOKEN'
+token = ENV['TOKEN']
+# token = App_Config.token
+reply = Reply.new
+scrapper = Scrapper.new      
 
+time = [%w(Launch Breakfast), %w(Dinner)]
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text:"Hi, #{message.from.first_name} "+reply.reply_txt['welcome'])
+    when '/ask'
+      bot.api.send_message(chat_id: message.chat.id, text: reply.reply_txt['ask_category'], reply_markup: reply.keyboard('ask_category'))
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+    if scrapper.is_category(message.to_s)
+      bot.api.send_message(chat_id: message.chat.id, 
+        text: reply.reply_txt['suggestions'] , 
+        reply_markup: reply.keyboard(scrapper.extract_info(message.to_s)))
+    elsif scrapper.is_recipe(message.to_s)
+      bot.api.send_message(chat_id: message.chat.id, text: reply.reply_txt['recipe']) 
     end
   end
 end
