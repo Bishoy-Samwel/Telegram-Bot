62ce838252af7f93485d0f020f796b68cc2476a9
Bishoy-Samwel <bishoy.samwuel@gmail.com>
2021-03-09 15:59:39 +0200

    Add get_recipe to get recipe info, get_suggestions to get recipies for a category

--- lib/reply.rb
+++ lib/reply.rb
@@ -1,25 +1,38 @@
-require_relative 'scrapper'
+require_relative './scrapper'
+
 class Reply
-  attr_reader :reply_txt, :reply_keyboard
+  attr_reader :reply_txt, :reply_keyboard, :scrapper
   def initialize
+    @scrapper = Scrapper.new
     @reply_txt= {
       'welcome' => 'Welcome to Chefo I am a Chef bot, what can I do for you?',
       # rubocop:disable Layout/LineLength
       'commands' => "You can control me with these commands: \n /start - start the bot \n /stop - stop the bot \n /book - get a recommendation for a recipe of your preferred food category \n /help - see the list of available commands",
       # rubocop:enable Layout/LineLength
+      'suggestions' => 'Here are some suggestions :)',
       'ask_category' => 'What category of food would you prefer?',
+      'recipe'=> 'Here we are',
       'another_recipes'=> 'Would you like another suggestion?',
       'wrong_input' => "I'm afraid I can't help you with that...What can I do for you?"
     }
     @reply_keyboard={
-      'ask_category' => ,
+      'ask_category' => @scrapper.categories,
+      'show_suggestion' => 'This is my suggestions' ,
       'yes_No' => [%w[Yes], %w[No]]
     }
   end
 
 
+  def get_recipe (message)
+    @reply_txt['recipe'] = @scrapper.one_recipe_info(message)
+  end
+
+  def get_suggestions(message)
+    @reply_keyboard['show_suggestions'] = @scrapper.extract_info(message.to_s)
+  end
+
   def keyboard(keyboard)
-    array = reply_keyboard[keyboard]
+    array = keyboard.is_a?(String)? reply_keyboard[keyboard] : keyboard
     Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: array, one_time_keyboard: true)
   end
 
