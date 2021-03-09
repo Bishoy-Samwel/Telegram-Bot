530cc20edc0d7c461b92360e1e44d9580c3c0921
Bishoy-Samwel <bishoy.samwuel@gmail.com>
2021-03-09 22:55:05 +0200

    Edit start , suggest message

--- bin/main.rb
+++ bin/main.rb
@@ -12,13 +12,16 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text:"Hi, #{message.from.first_name}"+reply.reply_txt['welcome'])
+      bot.api.send_message(chat_id: message.chat.id, text:"Hi, #{message.from.first_name} "+reply.reply_txt['welcome'],reply_markup: reply.keyboard('commands'))
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    when '/ask'
+    when '/help'
+      bot.api.send_message(chat_id: message.chat.id, text: reply.reply_txt['commands'], 
+      reply_markup: reply.keyboard('commands'))
+    when '/suggest'
       bot.api.send_message(chat_id: message.chat.id, text: reply.reply_txt['ask_category'], reply_markup: reply.keyboard('ask_category'))
-
     end
+
     if reply.scrapper.is_category(message.to_s)
       reply.get_suggestions(message)
       bot.api.send_message(chat_id: message.chat.id, 
@@ -26,7 +29,7 @@ Telegram::Bot::Client.run(token) do |bot|
         reply_markup: reply.keyboard('show_suggestions'))
     elsif reply.scrapper.is_recipe(message.to_s)
       reply.get_recipe(message.to_s)
-      bot.api.send_message(chat_id: message.chat.id, text: reply.reply_txt['recipe']) 
+      bot.api.send_message(chat_id: message.chat.id, text: reply.reply_txt['recipe'], parse_mode: 'html')
     end
   end
 end
