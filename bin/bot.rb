113cefd22f8cbc1317f5c1028dba2658929492ab
Bishoy-Samwel <bishoy.samwuel@gmail.com>
2021-03-11 20:21:35 +0200

    Check if the message is nil before send it

--- bin/bot.rb
+++ bin/bot.rb
@@ -6,8 +6,11 @@ token = ENV['TOKEN']
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    respond(message.text, message.chat.id, message.from.first_name).each do |message_obj|
-      bot.api.send_message(message_obj)
+    unless message.nil?
+      message_objs = respond(message.text, message.chat.id, message.from.first_name)
+      message_objs&.each do |message_obj|
+        bot.api.send_message(message_obj)
+      end
     end
   end
 end
